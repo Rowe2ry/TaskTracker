95dae9054f3aaddb6e27a495b5068f38e1b8239c
Chris Rowe <100010401+Rowe2ry@users.noreply.github.com>
2022-06-13 17:57:29 -0400

    Added various componennts to the task tracker

---
+++ client/src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="onclick()" :style="{background: color}" class="btn">
+        {{text}}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: {
+                type: String
+            },
+            color: {
+                type: String
+            }
+        },
+        methods: {
+            onClick() {
+                console.log('click');
+            }
+        } 
+    }
+</script>
+
+<style scoped>
+
+</style>
